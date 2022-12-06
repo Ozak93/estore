04a77d7dcdc079425154d5fca1f1b8882c38671e
taya huthaifa <thuthaifa@everconnect.com>
2022-12-06 12:21:55 +0300

    category.rb added enum and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
   has_and_belongs_to_many :products
-  enum name: [:category1, :category2]
+  enum name: [:clothes, :shoes, :bags, :pc_parts, :books, :hand_made]
 end
